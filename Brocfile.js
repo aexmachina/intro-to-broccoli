38ba55360a9c5532e5f5237896cadf78abd7aa80
Simon Wade <simon.wade@gmail.com>
2014-09-02 21:10:54 +1000

    Example 3

--- Brocfile.js
+++ Brocfile.js
@@ -2,15 +2,13 @@
 var concat = require('broccoli-concat');
 var pickFiles = require('broccoli-static-compiler');
 var mergeTrees = require('broccoli-merge-trees');
+var compileSass = require('broccoli-sass');
 
 var scripts = concat('app/', {
   inputFiles: ['**/*.js'],
   outputFile: '/assets/scripts.js'
 });
-var styles = concat('app/styles', {
-  inputFiles: ['**/*.css'],
-  outputFile: '/assets/styles.css'
-});
+var styles = compileSass(['app/styles'], 'app.scss', '/assets/styles.css');
 var public = pickFiles('public', {
   srcDir: '.',
   destDir: '.'
